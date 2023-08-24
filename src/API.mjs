d80b0c647ff31c89ac1e6a81aa88880fff97b07f
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-24 20:19:03 +0200

    fix submit handling

--- src/API.mjs
+++ src/API.mjs
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL:"http://localhost:4000/api/"
+    baseURL:"http://localhost:4000/api/",
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
 })
--- src/components/NewStory.jsx
+++ src/components/NewStory.jsx
@@ -1,16 +1,39 @@
 import Checkboxes from "./Checkboxes" 
+import { useState } from "react"
+import { API } from "../API.mjs"
 export default () => {
 
+    const [data,setData] = useState({
+        title:"",
+        word_count:0
+    })
 
-    return <>
-    POOOOOP
-    <form>
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const title = event.target.title.value
+        const word_count = event.target.word_count.value
+        API.post("stories",data)
+    }
+    const handleChange = (event) => {
+        const value = event.target.value
+        setData({
+            ...data,
+            [event.target.name]:value
+        })
+    }
+
+    return <form onSubmit={handleSubmit}>
     <label htmlFor="title">Title</label>
-    <input name="title" type="text"></input>
-    <label htmlFor="wordcount">Wordcount</label>
-    <input name="wordcount" type="number"></input>
+    <input name="title" type="text" 
+    value={data.title} 
+    onChange={handleChange}/>
+    <label htmlFor="word_count">Wordcount</label>
+    <input name="word_count" type="number"
+    value={data.word_count}
+    onChange={handleChange}/>
     <Checkboxes name="genres"/>
+    <button type="submit">SUBMIT</button>
     </form>
     
-    </>
+    
 }
