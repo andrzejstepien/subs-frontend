7804be2eebcc52abf7d7b93963af1bcd1e441c1c
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-24 17:02:13 +0200

    add focus switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import './App.css'
 import Table from './components/Table'
+import Sidebar from './components/Sidebar'
+import NewSubmission from './components/NewSubmission.jsx'
 import { API } from './API.mjs'
 import { useState, useEffect } from 'react'
 
 function App() {
     const [data,setData] = useState([{}])
+    const [focus,setFocus] = useState("MAIN")
+    const pages = {
+      MAIN:<Table data={data}/>,
+      NEW:<NewSubmission/>
+    }
    useEffect(()=>{
      API.get("submissions").then(res=>{setData(res.data)})
    },[])
- 
+   const changeFocus = (focus) => {
+    setFocus(focus)
+   }
 
   
 
   return (
-    
-      <Table data={data}/>
+    <>
+      <div className="main-wrapper">
+      <Sidebar changeFocus={changeFocus} pageKeys={Object.keys(pages)}/> 
+      <div className="middle">
+      {pages[focus]}
+      </div>
+      </div>
+    </>
+      
     
   )
 }
