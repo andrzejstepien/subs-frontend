6d20303455b45e0220983e8aaffb8edf1942bb6a
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-24 17:01:25 +0200

    add selectStories db call

--- server/selectCalls.mjs
+++ server/selectCalls.mjs
@@ -4,3 +4,8 @@ export const selectSubmissions = async () => {
     return db('submissions')
     .select('*')
 }
+
+export const selectStories = async () => {
+    return db('stories')
+    .select('title')
+}
