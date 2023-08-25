003bcd82df2794235be02050e7b9ee2548582322
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-25 14:07:18 +0200

    create genre badges component

--- src/components/GenreBadges.jsx
+++ src/components/GenreBadges.jsx
@@ -1,3 +1,17 @@
-export default () => {
+import { API } from "../API.mjs"
+import { useEffect, useState } from "react"
+
+export default (story) => {  
+    const [genres,setGenres] = useState([])
+    useEffect(()=>{
+        API.get('stories-genres')
+        .then(res=>{
+            setGenres(res.data)
+        })
+    },[])
     
+    const genresRendered = genres.map(genre=>{
+        return <span className="genre">{genre}</span>
+    })
+    return <span>{genresRendered}</span>
 }
