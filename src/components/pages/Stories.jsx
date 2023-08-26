24d79e630d535e0116a876dd61bcb5b3d1c70f75
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-26 23:53:27 +0200

    filter unwanted colums from Stories view

--- src/components/pages/Stories.jsx
+++ src/components/pages/Stories.jsx
@@ -8,16 +8,25 @@ export default (props) => {
     useEffect(()=>{
       API.get("page/stories").then(res=>{
         console.dir(res.data)
-        return setData(res.data)
+        return setData(removePairs(res.data,filterList))
       })  
       },[])
 
+      const filterList = [
+        'Submissions']
       
 
-
+      const removePairs = (array,keys) => {
+        return array.map(e=>{
+          for (const key of keys) {
+            delete e[key]
+          }
+          return e
+        })
+      }
 
     return  <Page heading="Stories">
-              <Table data={data} handleClick={props.handleClick}/>
+              <Table data={data} setFocus={props.setFocus}/>
             </Page>
     
    
