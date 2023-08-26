ed595772e4539850f303e05f683d75a5db2b48fa
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-26 11:52:54 +0200

    improve readability

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,6 +1,7 @@
 export default (props) => {
     
-    return <span className="badge-container">{props.array.map((e,i)=>{
-        return <span className="badge" key={i}>{e[0]}</span>
-    })}</span>
+    const badges = props.array.map((e,i)=>{
+        return <span className="badge" key={i}>{e}</span>
+    })
+    return <span className="badge-container">{badges}</span>
 }
