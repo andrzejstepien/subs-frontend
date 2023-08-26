78e1cb12f531c7b092d31482168a38e44fdbd576
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-26 23:52:57 +0200

    create singleStoryPage

--- src/components/pages/SingleStory.jsx
+++ src/components/pages/SingleStory.jsx
@@ -1,8 +1,20 @@
 import Page from "./Page"
-
+import Badges from "../Badges"
+import Table from "../Table"
 export default (props) => {
-    
-    return <Page heading={props.data.Title}>
-        {JSON.stringify(props.data)}
+    const data = props.data
+    return <Page heading={data.Title}>
+        <div className="storyContainer">
+            <div className="left">
+                <div><h3>ID:</h3> {data.ID}</div>
+                <div><h3>Wordcount: </h3> {data.Wordcount}</div>
+                <div><h3>Genres: </h3><Badges data={data.Genres} /></div>
+            </div>
+            <div className="right">
+                <h3>Submissions</h3>
+                {console.log(data?.Submissions)}
+                <Table data={data?.Submissions??[]} setFocus={props.setFocus}/>
+            </div>
+        </div>
     </Page>
 }
