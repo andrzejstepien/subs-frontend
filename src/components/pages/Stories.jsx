2ba79405ef0680acff62c67977a853cc6650f5f2
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-26 11:53:24 +0200

    remove unneccessary state

--- src/components/pages/Stories.jsx
+++ src/components/pages/Stories.jsx
@@ -3,13 +3,15 @@ import Table from "../Table"
 import {API} from '../../API.mjs'
 export default () => {
     const [data,setData] = useState([{}])
-    const [stories,setStories] = useState([])
     const [storyGenres,setStoryGenres] = useState({})
-
+  
     useEffect(()=>{
       API.get("stories-genres").then(res=>{setStoryGenres(res.data)})  
       API.get("stories/full").then(res=>{setData(res.data)})
       },[])
+    useEffect(()=>{
+      console.dir(storyGenres)
+    },[storyGenres])
 
 
 
