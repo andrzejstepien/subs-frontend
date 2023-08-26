a3957c01953933d179a9b870e43810ceaf823c28
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-26 18:10:01 +0200

    more flexible cell rendering

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,12 +4,16 @@ export default (props) => {
   //props.data ~ [{row1},{row2},{etc}]
   //each row ~ [{id:1, title:"The Red Room", etc},
   //each row ~ {id:1, title:"The Signalman", etc}]
-
+console.dir(props.data)
   const oddOrEven = (n) => {
     return ["evenRow", "oddRow"][n % 2]
   }
 
-
+const renderCellContents = (data) => {
+  if(Array.isArray(data)){
+    return <Overlay data={data}/>
+  } return <button onClick={()=>{props.handleClick(data)}}>{data}</button>
+}
 
   const tableHeaders = <tr className="rowHeader">
     {Object.keys(props.data[0]).map((heading, i) => {
@@ -19,11 +23,9 @@ export default (props) => {
 
   const tableRows = props.data.map((row, i) => {
     const cells = Object.keys(row).map((key, j) => {
-      const cellValue = row[key]
-      const overlaysValue = props.overlays ? props?.overlays[cellValue]:false
-      return <td key={"" + i + j}>
-        {overlaysValue &&<Overlay array={overlaysValue}/>} 
-        
+      const cellValue = renderCellContents(row[key])
+
+      return <td key={"" + i + j}> 
         {cellValue}
       </td>
     })
