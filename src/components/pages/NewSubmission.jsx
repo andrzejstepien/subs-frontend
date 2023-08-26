4aa5a769314a2daea32cbb1b69201b1db0203af4
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-26 18:16:31 +0200

    add page component

--- src/components/pages/NewSubmission.jsx
+++ src/components/pages/NewSubmission.jsx
@@ -1,9 +1,11 @@
 import Dropdown from "../Dropdown"
+import Page from "./Page"
 export default () => {
     
 
 
     return <>
+    <Page heading="New Submission">
     <form>
     <label htmlFor="stories">Story:</label>
     <Dropdown name="stories" />
@@ -11,6 +13,6 @@ export default () => {
     <Dropdown name="publishers"/>
     
     </form>
-    
+    </Page>
     </>
 }
