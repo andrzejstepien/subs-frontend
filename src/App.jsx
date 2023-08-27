88fd9f9dfb56b9b209d1cb084d1583d3c0f81c18
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-27 11:37:24 +0200

    centralise state handling

--- src/App.jsx
+++ src/App.jsx
@@ -9,45 +9,46 @@ import { useState, useEffect } from 'react'
 import { API } from './API.mjs'
 
 function App() {
+  const [storiesPageData, setStoriesPageData] = useState([])
   const [focus, setFocus] = useState("STORIES")
-  const [pageMatrix, setPageMatrix] = useState({
-    SUBMIT: <NewSubmission />,
-    "NEW STORY": <NewStory />,
-    STORIES: <Stories setFocus={setFocus} />,
-    OVERVIEW: <Overview setFocus={setFocus} />
-  })
-  const [storiesPageData,setStoriesPageData] = useState([])
-  
+  const [pageMatrix, setPageMatrix] = useState({})
+
+
   useEffect(() => {
 
-    API.get('page/stories').then(res=>{
+    API.get('page/stories').then(res => {
       const rows = res.data
       setStoriesPageData(rows)
-      const pages = {}
-      for (const row of rows) {
-        pages[row.Title] = <SingleStory data={row}/>
+      
+    })
+  }, [])
+  useEffect(() => {
+    const pages = {}
+      for (const row of storiesPageData) {
+        pages[row.Title] = <SingleStory data={row} />
       }
-      setPageMatrix(prev=>{
-        return{
-          ...prev,
+      setPageMatrix(prev => {
+        return {
+          SUBMIT: <NewSubmission />,
+          "NEW STORY": <NewStory />,
+          STORIES: <Stories data={storiesPageData} setFocus={setFocus} />,
+          OVERVIEW: <Overview setFocus={setFocus} />,
           ...pages
         }
       })
       console.dir(pageMatrix)
-    })
-  }, [])
+  },[storiesPageData])
 
 
 
 
 
-  
 
 
   return (
     <>
       <div className="main-wrapper">
-        <Sidebar setFocus={setFocus} pageKeys={Object.keys(pageMatrix).slice(0,4)} />
+        <Sidebar setFocus={setFocus} pageKeys={Object.keys(pageMatrix).slice(0, 4)} />
         <div className="middle">
           {pageMatrix[focus]}
         </div>
